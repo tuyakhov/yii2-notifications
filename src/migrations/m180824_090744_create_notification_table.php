c5ee821533351fccf391fd64f6dee6ad32e0baae
ssv <ssv@medincom.info>
2018-11-21 14:38:33 +0500

    namespaced migrations

--- src/migrations/m180824_090744_create_notification_table.php
+++ src/migrations/m180824_090744_create_notification_table.php
@@ -3,6 +3,8 @@
  * @copyright Anton Tuyakhov <atuyakhov@gmail.com>
  */
 
+namespace tuyakhov\notifications\migrations;
+
 use yii\db\Migration;
 
 class m180824_090744_create_notification_table extends Migration
